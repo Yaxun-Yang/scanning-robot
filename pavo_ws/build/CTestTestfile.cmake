ca3164e08ea428fe34a5cc4d266a6ff6642004be
RigerLee <ruijian.li@stereye.com>
2022-01-13 00:13:26 +0800

    clean

--- pavo_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stereye/pavo_ws/src
-# Build directory: /home/stereye/pavo_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("pavo_ros")
--- pavo_ws/src/pavo_ros/sdk/src/utils.cpp
+++
@@ -1,3 +0,0 @@
-#include "../include/utils.h"
-
-pavo::SinuLookupTable*  pavo::SinuLookupTable::instance_ = NULL;
