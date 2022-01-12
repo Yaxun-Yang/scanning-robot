cea60c5468ba8fd32777e18659566d48b0a89618
RigerLee <ruijian.li@stereye.com>
2022-01-13 00:14:04 +0800

    clean

--- mr_ws/src/mapping_robot/src/parser_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "BagParser.h"
-
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "recorder_node");
-    ros::NodeHandle nh("~");
-    auto mpParser = std::make_shared<stereye::BagParser>(nh);
-    mpParser->start();
-    return 0;
-}
--- mr_ws/src/mapping_robot/src/recorder_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "BagRecorder.h"
-
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "recorder_node");
-    ros::NodeHandle nh("~");
-    auto mpRecorder = std::make_shared<stereye::BagRecorder>(nh);
-    mpRecorder->start();
-    return 0;
-}
